19bdb570680fe58ccf76364beccc9fff79511728
liuxili01 <liuxili@bestseller.com.cn>
2022-04-26 16:30:32 +0800

    webflux file filter

--- rookie-webflux-filter/src/main/java/com/rookie/bigdata/handler/HelloWorldHandler.java
+++ rookie-webflux-filter/src/main/java/com/rookie/bigdata/handler/HelloWorldHandler.java
@@ -23,10 +23,51 @@
 //
 //        //        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
 ////                .body(BodyInserters.fromObject("Hello, 中国webflux !"));
-//
+////
 //        //解决中文乱码的问题
 //        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).header("Content-Type", "text/plain; charset=utf-8")
 //                //.body(BodyInserters.fromObject("Hello, 中国webflux !"));
 //                .body(BodyInserters.fromObject("Hello RouterFunction, 中国webflux !"));
+//
+//
+////        //http://localhost:9000/routehello?name=other
+////        //http://localhost:9000/routehello?name=123
+////
+////        String name = request.queryParam("name").orElse("other");
+////
+////        return mono(name).flatMap(serverResponse -> ServerResponse.ok()
+////                .contentType(MediaType.TEXT_PLAIN).syncBody(serverResponse))
+////                .onErrorResume(e -> Mono.just("Error: " + e.getMessage()).
+////                        flatMap(serverResponse -> ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+////                                .syncBody(serverResponse)));
+//
+//
+////        String name = request.queryParam("name").orElse("other");
+////        return mono(name)
+////                //.flatMap(serverResponse -> ServerResponse.ok()
+////                //.contentType(MediaType.TEXT_PLAIN).syncBody(serverResponse))
+////                .onErrorReturn("onErrorReturn").
+////                        flatMap(serverResponse -> ServerResponse.ok()
+////                                .contentType(MediaType.TEXT_PLAIN)
+////                                .syncBody(serverResponse));
+//
+//
 //    }
+//
+//    public Mono<String> mono(String value) {
+//
+//        switch (value) {
+//            case "abc":
+//                return Mono.just("a-b-c");
+//            case "bdc":
+//                return Mono.just("b-c-d");
+//            case "other":
+//                throw new NullPointerException("空指针异常");
+//            default:
+//                return Mono.just(value);
+//        }
+//
+//
+//    }
+//
 //}
